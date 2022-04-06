fbaa4a40c05c2718e1a141611382765d2f810f21
tejasmas <tejasmas@deloitte.com>
2022-04-06 11:46:27 +0530

    Assignment 4, Lambda problem 2

--- Mini Assignment 4/1Lambda_prob.py
+++ Mini Assignment 4/1Lambda_prob.py
@@ -1,7 +1,17 @@
-def prob1():
+def prob1(a, b, c, x):
     soln = lambda a, b, c, x: (a * (x ^ 2)) + (b * x) + c
 
-    return soln(1, 2, 3, 4)
+    return soln(a, b, c, x)
 
 
+def prob2():
+    lst1 = ["Alaska", "Alabama", "Arizona", "Arkansas", "Colorado", "Montana", "Nevada"]
 
+    result = list(map(lambda x: (x.count("a") + x.count("A")), lst1))
+
+    return result
+
+
+print("Solution : ", prob1(2, 5, 4, 2))
+
+print("The number of occurrences of 'a' or 'A' is : ", prob2())

716e6bc4c4293170edd27059394f8fece7c6f3bd
tejasmas <tejasmas@deloitte.com>
2022-04-08 01:15:00 +0530

    Python Track Main Assignment Added user rating to the movie details

--- Main Assignment/Base.py
+++ Main Assignment/Base.py
@@ -70,6 +70,17 @@ def user_display(title):
 
     return func
 
+def ratings():
+    movies = ("RRR", "83", "RX100")
+    for movie in movies:
+        print("Rating For Movie:", movie)
+        for i in range(len(movies)):
+            rating = int(input("Enter Movie rating: "))
+            if rating < 1 or rating > 10:
+                print("That's not a Valid number")
+                continue
+            break
+
 def book_ticket(title):
     lis = list(movie_file[title])
 
